ee3883bfe2b2981deb4128a0b528148a9307d22e
Mint <mintnguyen.pa@gmail.com>
2023-05-28 20:23:50 -0700

    feat: add register endpoint

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class RegisterAPIView(APIView):
+
+    def post(self, request):
+        return Response(request.data)
