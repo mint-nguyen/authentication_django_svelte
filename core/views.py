5702c41dc13b8e6a8153d25052728ee0fd5637fe
Mint <mintnguyen.pa@gmail.com>
2023-07-02 11:27:33 -0700

    feat: user token table

--- core/views.py
+++ core/views.py
@@ -1,8 +1,9 @@
+import datetime
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import exceptions
 from core.serializers import UserSerializer
-from .models import User
+from .models import User, UserTokens
 from .auth_token import create_auth_token, create_refresh_token, JWTAuthentication, decode_refresh_token
 
 
@@ -37,6 +38,9 @@ class LoginAPIView(APIView):
         auth_token = create_auth_token(user.id)
         refresh_token = create_refresh_token(user.id)
 
+        UserTokens.objects.create(user_id=user.id, token=refresh_token,
+                                  expired_at=datetime.datetime.utcnow() + datetime.timedelta(days=7))
+
         response = Response()
         response.set_cookie(key='refresh_token',
                             value=refresh_token, httponly=True)
@@ -58,6 +62,9 @@ class RefreshAPIView(APIView):
     def post(self, request):
         refresh_token = request.COOKIES['refresh_token']
         id = decode_refresh_token(refresh_token)
+
+        if not UserTokens.objects.filter(user_id=id, token=refresh_token, expired_at__gt=datetime.datetime.now(tz=datetime.timezone.utc)).exists():
+            raise exceptions.AuthenticationFailed('Refresh token expired!')
         access_token = create_auth_token(id)
         return Response({
             'token': access_token
@@ -66,6 +73,8 @@ class RefreshAPIView(APIView):
 
 class LogoutAPIView(APIView):
     def post(self, request):
+        refresh_token = request.COOKIES['refresh_token']
+        UserTokens.objects.filter(token=refresh_token).delete()
         response = Response()
         response.delete_cookie(key='refresh_token')
 
